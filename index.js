8bbf033ddfc835e55400e9120c9a93f70b391cac
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-02 22:46:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
 
+  yearsExperienceFromBeginningOf(year) {
+    let enteredYear = new Date(year, 1, 1);
+    let totalYears = (enteredYear - this.startDate) / (365*24*60*60*1000);
+    return parseInt(totalYears);
+  };
+};
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let horizontalBlocks = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    let verticalBlocks = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    return horizontalBlocks + verticalBlocks;
+  };
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    };
+  };
+};
