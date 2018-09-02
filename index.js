89ecbd00e6857bd3ec127ea9db3c30c11b7abc81
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-02 21:26:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
+
+  yearsExperienceFromBeginningOf(year) {
+    let presentDate = new Date(year, 1, 1);
+    let totalYears = (presentDate - this.startDate) / (365*24*60*60*1000);
+    return parseInt(totalYears);
+  };
+};
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+
+  };
+};
