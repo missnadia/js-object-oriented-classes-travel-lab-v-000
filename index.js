faf2060a5d9f6e3b7817cc8c826e39b1e0b05d77
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-02 22:30:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,15 +18,7 @@ class Route {
   };
 
   blocksTravelled() {
-    let eastWest = [
-      '1st Avenue',
-      '2nd Avenue',
-      '3rd Avenue',
-      'Lexington Avenue',
-      'Park',
-      'Madison Avenue',
-      '5th Avenue'
-    ];
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
     let horizontalBlocks = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal);
     let verticalBlocks = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
     return (Math.abs(horizontalBlocks) + verticalBlocks);
